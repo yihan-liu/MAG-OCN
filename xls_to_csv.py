0c262eb9d20ada693f369e1b7cfc6fd95266b7d4
yihan-liu <y1hliu@unc.edu>
2025-02-08 13:09:06 -0500

    add argparse to csv conversion tool

--- xls_to_csv.py
+++ xls_to_csv.py
@@ -1,3 +1,7 @@
+# xls_to_csv.py
+
+import argparse
+
 import pandas as pd
 
 def xlsx_to_multiple_csv(xlsx_path):
@@ -15,7 +19,10 @@ def xlsx_to_multiple_csv(xlsx_path):
         print(f"Sheet '{sheet_name}' has been saved to '{csv_filename}'")
 
 # Example usage
-if __name__ == "__main__":
-    path_to_excel = "original_dataset/CN-origin_Cartesian.xlsx"
-    xlsx_to_multiple_csv(path_to_excel)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--filepath', default=None,
+                        help='Path to the xlsx file to convert.')
+    args = parser.parse_args()
+    xlsx_to_multiple_csv(args.filepath)
 
